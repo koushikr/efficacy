ae7a6563897c3a89a740a9b4fc75aca492b07f04
Koushik Ramachandra <koushiksmiles@gmail.com>
2016-05-19 18:34:35 +0530

    No Args constructor added

--- src/main/java/com/github/koushikr/models/InboundEntity.java
+++ src/main/java/com/github/koushikr/models/InboundEntity.java
@@ -2,6 +2,7 @@ package com.github.koushikr.models;
 
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
@@ -13,6 +14,7 @@ import javax.persistence.Table;
 @Setter
 @Getter
 @Entity
+@NoArgsConstructor
 @Table(name = "inbound_messages")
 public class InboundEntity extends CallEntity {
 
